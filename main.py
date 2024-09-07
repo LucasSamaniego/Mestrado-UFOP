c1f15fcfd43f6ec131409792a7ec4f0da7bbfd5f
LucasSamaniego <samaniego444@gmail.com>
2024-09-06 22:46:39 -0300

    Alterações pequenas

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ st.title("Gestão de armazém")
 # Criar o botão
 if st.button("Clique aqui"):
     # Chamar a função quando o botão for clicado
-    destino = SalaB
+    destino = "SalaB"
     atual = None
 
     while atual != destino:
         atual = navy.map(destino)
-        st.write(msg)
+        st.write(atual)
