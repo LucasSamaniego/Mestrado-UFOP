fd61973138bf2299ab4fa993e3c65dd0f26d36df
LucasSamaniego <samaniego444@gmail.com>
2024-09-06 23:07:18 -0300

    Ajuste

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import navegation as navy
 st.title("Gestão de armazém")
 
 # Criar o botão
-if st.button("Clique aqui"):
+if st.button("Modo Navegação"):
     # Chamar a função quando o botão for clicado
     destino = "SalaB"
     atual = None
@@ -14,3 +14,10 @@ if st.button("Clique aqui"):
     while atual != destino:
         atual = navy.map(destino)
         st.write(atual)
+
+
+if st.button("Modo Detalhamento"):
+
+    pn = st.text_input("Part Number", "PN")
+    sn = st.text_input("Serial Number", "SN")
+    st.write("The current movie title is", title)
